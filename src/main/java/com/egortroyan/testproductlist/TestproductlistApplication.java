0430eb77156e144b637d960409e59760ab29573a
EgorTroyan <etroyan@bk.ru>
2022-02-08 12:48:28 +0300

    new project

---
+++ src/main/java/com/egortroyan/testproductlist/TestproductlistApplication.java
@@ -0,0 +1,13 @@
+package com.egortroyan.testproductlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestproductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestproductlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egortroyan/testproductlist/TestproductlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egortroyan.testproductlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestproductlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
